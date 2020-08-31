64e70f3ff120b48c854be5bc128bd577be0d777a
Imgkl <gokul@1stmain.co>
2020-09-01 01:59:18 +0530

    added flare animation and animated texts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:imgkl/views/pages/page_second/landing_page.dart';
+import 'package:imgkl/views/landing_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,23 +18,50 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LandingPage(),
+      home: CheckDevice(),
     );
   }
 }
 
-// class CheckDevice extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final screenWidth = MediaQuery.of(context).size.width;
-//     return (screenWidth > 700 == true)
-//         ? LandingPage()
-//         : Scaffold(
-//             body: Container(
-//               child: Center(
-//                 child: Text("Visit from PC"),
-//               ),
-//             ),
-//           );
-//   }
-// }
+class CheckDevice extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return (screenWidth > 700 == true)
+        ? LandingPage()
+        : Scaffold(
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: screenHeight * 0.8,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 95.0),
+                    child: FlareActor(
+                      'assets/Filip.flr',
+                      animation: "idle",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 08.0),
+                  child: TyperAnimatedTextKit(
+                      text: [
+                        "Mobile site is under construction.\nPlease visit from PC",
+                      ],
+                      isRepeatingAnimation: false,
+                      textStyle:
+                          TextStyle(fontSize: 30.0,),
+                      textAlign: TextAlign.center,
+                      alignment:
+                          AlignmentDirectional.topStart
+                      ), 
+                ),
+              ],
+            ),
+          );
+  }
+}
