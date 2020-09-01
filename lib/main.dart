1c2daf8f176a482af8d2ecf0ad47e55e63951c3a
Imgkl <gokul@1stmain.co>
2020-09-02 01:57:04 +0530

    removed the expanded

--- lib/main.dart
+++ lib/main.dart
@@ -36,36 +36,30 @@ class CheckDevice extends StatelessWidget {
             body: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    height: screenHeight * 0.8,
-                    width: double.infinity,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 95.0),
-                      child: FlareActor(
-                        'assets/Filip.flr',
-                        animation: "idle",
-                        fit: BoxFit.cover,
-                      ),
+                Container(
+                  height: screenHeight * 0.8,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 95.0),
+                    child: FlareActor(
+                      'assets/Filip.flr',
+                      animation: "idle",
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 80.0),
-                    child: TyperAnimatedTextKit(
-                        text: [
-                          "Mobile site is under construction.\nPlease visit from PC",
-                        ],
-                        isRepeatingAnimation: false,
-                        textStyle: TextStyle(
-                          fontSize: 30.0,
-                        ),
-                        textAlign: TextAlign.center,
-                        alignment: AlignmentDirectional.topStart),
-                  ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 80.0),
+                  child: TyperAnimatedTextKit(
+                      text: [
+                        "Mobile site is under construction.\nPlease visit from PC",
+                      ],
+                      isRepeatingAnimation: false,
+                      textStyle: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                      textAlign: TextAlign.center,
+                      alignment: AlignmentDirectional.topStart),
                 ),
               ],
             ),
