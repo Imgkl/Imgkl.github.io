2829eeb07d988d47f9a0d21714748690799417fd
Imgkl <gokul@1stmain.co>
2020-09-02 01:14:10 +0530

    added canvas colors

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Gokul\'s Portfolio',
       theme: ThemeData(
+        canvasColor: Colors.white,
         fontFamily: "Alata",
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -31,34 +32,40 @@ class CheckDevice extends StatelessWidget {
     return (screenWidth > 700 == true)
         ? LandingPage()
         : Scaffold(
+            backgroundColor: Colors.white,
             body: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  height: screenHeight * 0.8,
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 95.0),
-                    child: FlareActor(
-                      'assets/Filip.flr',
-                      animation: "idle",
-                      fit: BoxFit.cover,
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    height: screenHeight * 0.8,
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 95.0),
+                      child: FlareActor(
+                        'assets/Filip.flr',
+                        animation: "idle",
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 08.0),
-                  child: TyperAnimatedTextKit(
-                      text: [
-                        "Mobile site is under construction.\nPlease visit from PC",
-                      ],
-                      isRepeatingAnimation: false,
-                      textStyle:
-                          TextStyle(fontSize: 30.0,),
-                      textAlign: TextAlign.center,
-                      alignment:
-                          AlignmentDirectional.topStart
-                      ), 
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 80.0),
+                    child: TyperAnimatedTextKit(
+                        text: [
+                          "Mobile site is under construction.\nPlease visit from PC",
+                        ],
+                        isRepeatingAnimation: false,
+                        textStyle: TextStyle(
+                          fontSize: 30.0,
+                        ),
+                        textAlign: TextAlign.center,
+                        alignment: AlignmentDirectional.topStart),
+                  ),
                 ),
               ],
             ),
